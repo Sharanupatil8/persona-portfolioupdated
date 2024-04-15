773c540c3e3f766dbf4542a323e66ab67249417f
Sharanu P <93566630+Sharanupatil8@users.noreply.github.com>
2024-04-15 15:57:50 +0530

    Update Works.jsx

--- src/Components/Works.jsx
+++ src/Components/Works.jsx
@@ -3,15 +3,17 @@ import { motion } from "framer-motion";
 import { style } from "../style";
 import { github } from "../assets";
 import { projects } from "../constants";
-import { fadeIn, textVariant } from "./utils/motion";
+import { textVariant } from "./utils/motion";
 import { SectionWrapper } from "./hoc";
 
-console.log(projects);
-
-const ProjectCard = ({ project, i }) => {
+function ProjectCard({ project, i }) {
   const { image, name, source_code_link, description, tags } = project;
+
   return (
-    <motion.div variants={fadeIn("up", "spring", i * 0.5, 0.75)}>
+    <motion.div
+      whileInView={{ y: [100, 0], opacity: [0, 1] }}
+      transition={{ duration: 0.3, delay: i * 0.1 }}
+    >
       <Tilt
         options={{
           max: 45,
@@ -39,7 +41,9 @@ const ProjectCard = ({ project, i }) => {
         </div>
         <div className="mt-5">
           <h3 className="font-bold text-white text-[24px]">{name}</h3>
-          <p className="text-secondary text-[14px] mt-2">{description}</p>
+          <p className="text-secondary text-[14px] mt-2 line-clamp-3">
+            {description}
+          </p>
         </div>
         <div className="flex flex-wrap mt-4 gap-2">
           {project.tags.map((tag, i) => {
@@ -53,25 +57,30 @@ const ProjectCard = ({ project, i }) => {
       </Tilt>
     </motion.div>
   );
-};
+}
 
 function Works() {
   return (
     <>
-      <motion.div variants={textVariant()}>
-        <p className={`${style.sectionSubText}`}>MY WORK</p>
+      <motion.div
+        whileInView={{ y: [-50, 0], opacity: [0, 100], type: "spring" }}
+        transition={{ duration: 0.2, delay: 0.4 }}
+      >
+        <p className={`${style.sectionSubText}`}>Our WORKs</p>
         <h2 className={`${style.sectionHeadText}`}>Projects.</h2>
       </motion.div>
       <div className="w-full flex">
         <motion.p
           className="text-secondary mt-4 text-[17px] max-w-3xl leading-[30px]"
-          variants={fadeIn("", "", 0.1, 1)}
+          whileInView={{ y: [-50, 0], opacity: [0, 100], type: "spring" }}
+          transition={{ duration: 0.2, delay: 0.4 }}
         >
-          Following projects showcases my skills and experience through
-          real-world examples of my work. Each project is briefly described with
-          links to code repositories and live demos in it. It reflects my
-          ability to solve complex problems, work with different technologies,
-          and manage projects effectively.
+          The following projects exemplify our agency's proficiency and
+          expertise through tangible demonstrations of our work in real-world
+          scenarios. Each project is succinctly outlined, accompanied by links
+          to code repositories and live demos. These showcases underscore our
+          capacity to tackle intricate challenges, adeptly navigate diverse
+          technologies, and efficiently oversee project management initiatives.
         </motion.p>
       </div>
 
